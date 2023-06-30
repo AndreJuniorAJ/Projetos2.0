161d2268aa173dd1c96e121a5983155849149c40
André Junior <andresinholxl@gmail.com>
2023-06-30 10:39:16 -0300

    código funcionando

--- udemy/Projeto_0/js/função.js
+++ udemy/Projeto_0/js/função.js
@@ -1,9 +1,35 @@
 //Selecionar os elementos de input 
-const emailInput = document.querySelector('#contact-email input');
-const passwordInput = document.querySelector('#contact-password input');
+const emailInput = document.querySelector('#input');
+const passwordInput = document.querySelector('#inputSenha');
 const button = document.querySelector('.btn-primary');
 const resultContainer = document.querySelector('.result-container');
 const linkContainer = document.querySelector('#link');
+const emailCadastro = document.querySelector('#emailCad');
+const senhaCadastro = document.querySelector('#senhaCad');
+const buttonSave = document.querySelector('#btn-save');
+
+function criarRegistro(userName, userEmail, userPassword){
+    const userRegister = userEmail + " , " + userPassword;
+    localStorage.setItem (userName, userRegister);
+};
+
+
+function salvarDado () {
+
+    buttonSave.addEventListener('click', function(e) {
+
+    const user = Math.floor(Math.random() * (10 - 0) + 0);
+    const emailSave = emailCadastro.value;
+    const senhaSave = senhaCadastro.value;
+    
+    criarRegistro(user, emailSave, senhaSave);
+    // Atualize a página
+    location.reload();
+        
+});
+};
+
+salvarDado();
 
 button.addEventListener("click", function(event){
     //Pegar os valores digitados
@@ -20,9 +46,5 @@ button.addEventListener("click", function(event){
         resultContainer.innerHTML = `Login inválido, tente novamente`;
         event.preventDefault();
     };
-
-    
  
 });
-
-
